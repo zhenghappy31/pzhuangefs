af35d509795701d1b23f4657dbdb4aade893daf1
zhenghappy31 <markzhuang@126.com>
2017-02-06 13:00:10 -0600

    Add files via upload

--- resources/views/customers/show.blade.php
+++ resources/views/customers/show.blade.php
@@ -41,9 +41,93 @@
 
 
             </tbody>
-        </table>
+      </table>
     </div>
+	<?php
+	 $stotal = 0;
+	 $itotal = 0;
+     $ivalue=0;
+	 ?>
+	 
+	<h2>Stocks </h2>
+	<div>
+	       <table class="table table-striped table-bordered table-hover">
+        <thead>
+        <tr class="bg-info">
+           
+            <th>Symbol</th>
+            <th>Stock Name</th>
+            <th>No. of Shares</th>
+            <th>Purchase Price</th>
+            <th>Purchase Date</th>
+			<th>Original Value</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($customer->stocks as $stock)
+            <tr>
+               
+                <td>{{ $stock->symbol }}</td>
+                <td>{{ $stock->name }}</td>
+                <td>{{ $stock->shares }}</td>
+                <td>{{ $stock->purchase_price }}</td>
+                <td>{{ $stock->purchased }}</td>
+				<td>{{$stock->shares * $stock->purchase_price}}
+				 <?php $stotal= $stotal+ ($stock->shares *  $stock->purchase_price)?>
+				
+                   
+        @endforeach
+
+        </tbody>
+
+    </table>
+	<h4><?php echo 'Total of Initial Stock Portfolio is  $', number_format($stotal,2);?> </h4>
+	
+</div>
+	<h2>Investments </h2>
+<div>
+<table class="table table-striped table-bordered table-hover">
+        <thead>
+        <tr class="bg-info">
+		   
+            <th>Category</th>
+            <th>Description</th>
+            <th>Acquired Value</th>
+            <th>Acquired Date</th>
+            <th>Recent Value</th>
+            <th>Recent Date</th>
+           
+        </tr>
+        </thead>
+        <tbody>
+        @foreach ($customer->investments as $investment)
+            <tr>
+               
+                <td>{{ $investment->category }}</td>
+                <td>{{ $investment->description }}</td>
+                <td>{{ $investment->acquired_value }}</td>
+                <td>{{ $investment->acquired_date }}</td>
+                <td>{{ $investment->recent_value }}</td>
+				<td>{{ $investment->recent_date }}</td>
+				  <?php $itotal= $itotal + $investment->acquired_value?>
+				  <?php $ivalue= $ivalue + $investment->recent_value?>
+
+              
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+	<h4><?php echo 'Total of Initial Investment Portfolio is  $', number_format($itotal,2);?> </h4>
+	<br>
+	<h3> Summary Of Portfolio</h3>
+	 <h4>
+                <?php echo 'Total of Initial Investment Portfolio $', number_format($itotal,2);?>
+                <br>
+				<br>
+                <?php echo 'Total Current Investment Portfolio $',number_format($ivalue,2)?>
+            </h4>
+
+	</div>
 
 @stop
-
 
